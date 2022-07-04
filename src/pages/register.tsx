4ad40b6b87c3e694a62613d0631e32387fe23e9a
Stjepan Nikolic <nikolicstjepansb@gmail.com>
2022-07-04 18:02:20 +0200

    google register

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
 import { trpc } from "../utils/trpc";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const Home: NextPage = () => {
   const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
@@ -9,18 +9,18 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Sikirevci nekad</title>
+        <title>Sikirevci nekad - Registracija</title>
         <meta name="description" content="Uspomene iz Sikirevaca" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <div className="container flex flex-col items-center justify-center min-h-screen p-10 px-0 mx-auto md:py-20 md:p-10 md:px-0">
-        <h1 className="font-extrabold text-center text-7xl">Sikirevci nekad</h1>
+        <h1 className="font-extrabold text-center text-7xl">Registracija</h1>
 
         <div className="py-6 text-2xl text-blue-500">
           {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
         </div>
-        <Link href="/register">Registracija</Link>
+        <button onClick={() => signIn()}>signIn</button>
       </div>
     </>
   );
