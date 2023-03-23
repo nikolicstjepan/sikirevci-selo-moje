69caecd28c6a139c589cdf44f6ee49de8f563933
Stjepan Nikolic <nikolicstjepansb@gmail.com>
2023-03-23 06:38:25 +0100

    categories and tags admin done

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -29,8 +29,10 @@ const MemoriesListPage: NextPage = () => {
       </Head>
 
       <MainLayout>
-        <MemoryCategories />
-        <MemoryTags />
+        <div className="flex gap-4 flex-wrap">
+          <MemoryCategories />
+          <MemoryTags />
+        </div>
       </MainLayout>
     </>
   );
@@ -38,97 +40,43 @@ const MemoriesListPage: NextPage = () => {
 
 function MemoryCategories() {
   const utils = trpc.useContext();
-  const list = trpc.admin.listMemoryCategories.useQuery();
-  const { mutateAsync: create, isLoading: isCreating } = trpc.admin.createMemoryCategory.useMutation();
-  const { mutateAsync: edit, isLoading: isEditing } = trpc.admin.editMemoryCategory.useMutation();
-  const { mutateAsync: remove } = trpc.admin.deleteMemoryCategory.useMutation();
-
-  const [name, setName] = useState("");
-  const [idToEdit, setIdToEdit] = useState("");
-
-  if (list.isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (list.isError) {
-    return <div>Error</div>;
-  }
-
-  if (!list.data) {
-    return <div>No data</div>;
-  }
-
-  const handleSubmit = async () => {
-    if (idToEdit) {
-      await edit({ id: idToEdit, name });
-      setIdToEdit("");
-    } else {
-      await create({ name });
-    }
-    utils.admin.listMemoryCategories.invalidate();
-    setName("");
-  };
-
-  const handleSetToEdit = (category: { id: string; name: string }) => {
-    setIdToEdit(category.id);
-    setName(category.name);
-  };
-
-  const handleRemove = async (id: string) => {
-    await remove({ id });
-    utils.admin.listMemoryCategories.invalidate();
-  };
 
   return (
-    <div>
-      <h1 className="text-3xl font-extrabold mb-4">Kategorije</h1>
-      <div className="mb-8">
-        {list.data.map((category) => {
-          return (
-            <div className="mb-1" key={category.id}>
-              {category.name}{" "}
-              <button className="text-blue font-bold mr-2" onClick={() => handleSetToEdit(category)}>
-                Uredi
-              </button>
-              <button className="text-red-700 font-bold" onClick={() => handleRemove(category.id)}>
-                Obriši
-              </button>
-            </div>
-          );
-        })}
-      </div>
-      <div className="mb-8 flex gap-2">
-        <input
-          type="text"
-          disabled={isCreating || isEditing}
-          value={name}
-          name="title"
-          onChange={({ target }) => setName(target.value)}
-          className="
-                    mt-1
-                    block
-                    rounded-md
-                    border-gray-300
-                    focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
-                  "
-        />
-        <button disabled={isCreating || isEditing || !name} onClick={handleSubmit} className="btn btn-primary">
-          {idToEdit ? "Spremi" : "Dodaj"}
-        </button>
-      </div>
-    </div>
+    <MemoryMeta
+      title="Kategorije"
+      getList={trpc.admin.listMemoryCategories}
+      getCreate={trpc.admin.createMemoryCategory}
+      getEdit={trpc.admin.editMemoryCategory}
+      getDelete={trpc.admin.deleteMemoryCategory}
+      getInvalidate={utils.admin.listMemoryCategories}
+    />
   );
 }
 
 function MemoryTags() {
   const utils = trpc.useContext();
-  const list = trpc.admin.listMemoryTags.useQuery();
-  const { mutateAsync: create, isLoading: isCreating } = trpc.admin.createMemoryTag.useMutation();
-  const { mutateAsync: edit, isLoading: isEditing } = trpc.admin.editMemoryTag.useMutation();
-  const { mutateAsync: remove } = trpc.admin.deleteMemoryTag.useMutation();
+
+  return (
+    <MemoryMeta
+      title="Tagovi"
+      getList={trpc.admin.listMemoryTags}
+      getCreate={trpc.admin.createMemoryTag}
+      getEdit={trpc.admin.editMemoryTag}
+      getDelete={trpc.admin.deleteMemoryTag}
+      getInvalidate={utils.admin.listMemoryTags}
+    />
+  );
+}
+
+function MemoryMeta({ getList, getCreate, getEdit, getDelete, title, getInvalidate }) {
+  const list = getList.useQuery();
+  const { mutateAsync: create, isLoading: isCreating } = getCreate.useMutation();
+  const { mutateAsync: edit, isLoading: isEditing } = getEdit.useMutation();
+  const { mutateAsync: remove } = getDelete.useMutation();
 
   const [name, setName] = useState("");
   const [idToEdit, setIdToEdit] = useState("");
+  const [idToDelete, setIdToDelete] = useState("");
 
   if (list.isLoading) {
     return <div>Loading...</div>;
@@ -149,7 +97,7 @@ function MemoryTags() {
     } else {
       await create({ name });
     }
-    utils.admin.listMemoryTags.invalidate();
+    getInvalidate.invalidate();
     setName("");
   };
 
@@ -159,23 +107,33 @@ function MemoryTags() {
   };
 
   const handleRemove = async (id: string) => {
+    if (!idToDelete) {
+      setIdToDelete(id);
+
+      return;
+    }
+
+    if (idToDelete !== id) {
+      setIdToDelete(id);
+      return;
+    }
     await remove({ id });
-    utils.admin.listMemoryTags.invalidate();
+    getInvalidate.invalidate();
   };
 
   return (
     <div>
-      <h1 className="text-3xl font-extrabold mb-4">Oznake</h1>
+      <h1 className="text-3xl font-extrabold mb-4">{title}</h1>
       <div className="mb-8">
-        {list.data.map((tag) => {
+        {list.data.map((item) => {
           return (
-            <div className="mb-1" key={tag.id}>
-              {tag.name}{" "}
-              <button className="text-blue font-bold mr-2" onClick={() => handleSetToEdit(tag)}>
+            <div className="mb-1" key={item.id}>
+              {item.name}{" "}
+              <button className="text-blue font-bold mr-2" onClick={() => handleSetToEdit(item)}>
                 Uredi
               </button>
-              <button className="text-red-700 font-bold" onClick={() => handleRemove(tag.id)}>
-                Obriši
+              <button className="text-red-700 font-bold" onClick={() => handleRemove(item.id)}>
+                {idToDelete === item.id ? "Potvrdi" : "Obriši"}
               </button>
             </div>
           );
@@ -196,11 +154,12 @@ function MemoryTags() {
                     focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
                   "
         />
-        <button disabled={isCreating || isEditing || !name} onClick={handleSubmit} className="btn btn-primary">
+        <button disabled={isCreating || isEditing || !name} onClick={handleSubmit} className="btn btn-sm text-blue">
           {idToEdit ? "Spremi" : "Dodaj"}
         </button>
       </div>
     </div>
   );
 }
+
 export default MemoriesListPage;
